51fd7c8b6a6d59b115a58e283771f872d03fdd31
BlP. <dev.sinanp@gmail.com>
2024-05-04 01:18:37 -0700

    up api

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import Home from "./Page/Home/Home";
-import Navbar from "./Components/Navbar/Navbar";
-import Footer from "./Components/Footer/Footer";
-import Dashbord from "./Page/Dashbord/Dashbord"
+import Home from "./Page/Home";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import { Route, Routes} from "react-router-dom";
 
 export default function App() {
@@ -11,7 +10,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/dashbord" element={<Dashbord/>} />
+        <Route path="/login" element={} />
       </Routes>
       <Footer />
     </>
---
+++ Frontend/src/Components/CardDetail/index.jsx
@@ -0,0 +1,100 @@
+import {
+    Card,
+    CardContent,
+    Typography,
+    CardActions,
+    Button,
+    CardMedia,
+    Rating,
+    Box,
+  } from "@mui/material";
+  import StarIcon from "@mui/icons-material/Star";
+  import React, { useState } from "react";
+  
+  export default function CardDetail({ name, img, current, brandName }) {
+    const [valueRating, setValueRating] = useState(3);
+    return (
+      <div>
+        <Card
+          sx={{
+            width: "346px",
+            height: "438px",
+            borderRadius: "10px",
+            p: "20px",
+          }}
+        >
+           <CardMedia
+            component="img"
+            alt="green iguana"
+            width="336px"
+            height="244px"
+            image={`https://${img}`}
+            sx={{ borderRadius: "10px" }}
+          />
+          <CardContent>
+            <Box display={"flex"} justifyContent={"space-between"}>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                sx={{ fontFamily: "Acme" }}
+              >
+                {name.substring(0, 20)}
+              </Typography>
+              <Rating
+                name="hover-feedback"
+                value={valueRating}
+                emptyIcon={<StarIcon sx={{ opacity: 0.55 }} />}
+                onChange={(event, newHaver) => {
+                  setValueRating(newHaver);
+                }}
+                precision={1}
+              />
+            </Box>
+            <Typography
+              gutterBottom
+              component="div"
+              sx={{
+                fontFamily: "Acme",
+                fontSize: "12px",
+                color: "text.secondary",
+              }}
+            >
+              {brandName}
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="body2"
+              sx={{ fontFamily: "Acme", fontWeight: 600 }}
+            >
+              (4.1k) Customer Reviews
+            </Typography>
+          </CardContent>
+          <CardActions
+            sx={{ display: "flex", justifyContent: "space-between", p: 2 }}
+          >
+            <Typography gutterBottom sx={{ fontFamily: "Acme" }}>
+              {current}
+            </Typography>
+            <Button
+              variant="contained"
+              size="large"
+              sx={{
+                bgcolor: "#FFAFCF",
+                borderRadius: 5,
+                color: "black",
+                "&:hover": { bgcolor: "#FFAFCF" },
+                fontFamily: "Acme",
+                fontSize: "10px",
+              }}
+            >
+              Buy
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
+  
+  
+  
