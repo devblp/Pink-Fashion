bb859fa518b23e2d45b87e41319ac8dd24b81c2f
BlP. <dev.sinanp@gmail.com>
2024-05-12 13:35:05 -0700

    add

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,35 +2,55 @@ import React from "react";
 import Home from "./Page/Home";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import Abaut from "./Page/Abaut"
-import Cart from "./Page/Cart"
-import Dasbord from "./Page/Dashbord"
-import ProductDetail from "./Page/ProductDetail"
+import Abaut from "./Page/Abaut";
+import Cart from "./Page/Cart";
+import Dasbord from "./Page/Dashbord";
+import ProductDetail from "./Page/ProductDetail";
 import Products from "./Page/Products";
-import NotPage404 from "./Page/NotPage404"
+import NotPage404 from "./Page/NotPage404";
 import Auth from "./Page/Auth";
-import SignUp from "./Page/Auth/SignUp"
-import { Navigate, Route, Routes} from "react-router-dom";
-import {useSelector} from "react-redux"
-
+import SignUp from "./Page/Auth/SignUp";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { createTheme, ThemeProvider } from "@mui/material";
 export default function App() {
-  const {token} = useSelector(state=>state.auth)
+  const { token } = useSelector((state) => state.auth);
+  const theme = createTheme({
+    palette:{
+      primary: {
+        mn: "#ff92bc",
+        bk:"#000000",
+        ws:"#ffffff",
+      },
+    }
+  });
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/abaut" element={<Abaut/>} />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/dashbord" element={token?<Dasbord/>:<Navigate to={"/auth"}/>}/>
-        <Route path="/product-detail/:id/name" element={<ProductDetail/>}/>
-        <Route path="/products" element={<Products/>} />
-        <Route path="/auth" element={!token?<Auth/>:<Navigate to={"/dashbord"}/>}/>
-        <Route path="/search/:query" element={!token?<Auth/>:<Navigate to={"/"}/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path="*" element={<NotPage404/>}/>
-      </Routes>
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/abaut" element={<Abaut />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/dashbord"
+            element={token ? <Dasbord /> : <Navigate to={"/auth"} />}
+          />
+          <Route path="/product-detail/:id/name" element={<ProductDetail />} />
+          <Route path="/products" element={<Products />} />
+          <Route
+            path="/auth"
+            element={!token ? <Auth /> : <Navigate to={"/dashbord"} />}
+          />
+          <Route
+            path="/search/:query"
+            element={!token ? <Auth /> : <Navigate to={"/"} />}
+          />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="*" element={<NotPage404 />} />
+        </Routes>
+        <Footer />
+      </ThemeProvider>
     </>
   );
 }
--- Frontend/src/Components/Navbar/index.jsx
+++ Frontend/src/Components/Navbar/index.jsx
@@ -1,122 +1,39 @@
-import { Button, Box, MenuItem, Menu, Typography, Toolbar, AppBar, Avatar } from "@mui/material";
+import {
+  Button,
+  Box,
+  MenuItem,
+  Menu,
+  Typography,
+  Toolbar,
+  AppBar,
+  Avatar,
+} from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
 import { Fragment } from "react";
 import "./style.css";
-import {useNavigate, Link } from "react-router-dom";
-import {useSelector} from "react-redux"
+import { useNavigate, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Footer() {
-  const {token,user,avatar} = useSelector(state=>state.auth)
-  const navigate = useNavigate()
+  const { token, user, avatar } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
   return (
-    <PopupState variant="popover" popupId="demo-popup-menu">
-      {(PopupState) => (
-        <Fragment>
-          <Box>
-            <AppBar
-              position="static"
-              sx={{
-                backgroundColor: "#1F1F1F",
-                boxShadow: "none",
-                padding: 2,
-              }}
-            >
-              <Toolbar
-                sx={{
-                  boxShadow: "none",
-                  display: "flex",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Box>
-                  <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
-                    logo
-                  </Typography>
-                </Box>
-                <Box sx={{ display: "flex",background:"#323232d7", borderRadius:18, padding:"0px 50px 0px 50px" }}>
-                  <Button
-                    disableRipple
-                    sx={{
-                      my: 2,
-                      margin:0,
-                      padding:2,
-                      color: "white",
-                      display: "block",
-                      ":hover": { color: "#FFAFCF",backgroundColor:"rgba(0, 0, 0, 0)", },
-                    }}
-                  >
-                    <Link className="link-navbar" to={"/"}>Home</Link>
-                  </Button>
-                  <Button
-                    disableRipple
-                    sx={{
-                      my: 2,
-                      margin:0,
-                      padding:2,
-                      color: "white",
-                      display: "block",
-                      ":hover": {
-                        color: "#FFAFCF",
-                        backgroundColor:"rgba(0, 0, 0, 0)",
-                      },
-                    }}
-                    {...bindTrigger(PopupState)}
-                  >
-                    <Link className="link-navbar" to={"/products/:detailId/:detailName"}>Shop</Link>
-                  </Button>
-                  <Menu {...bindMenu(PopupState)}>
-                    <MenuItem
-                      onClick={PopupState.close}
-                    >
-                      Profile
-                    </MenuItem>
-                    <MenuItem  onClick={PopupState.close}>ss</MenuItem>
-                    <MenuItem onClick={PopupState.close}>Logout</MenuItem>
-                    <MenuItem onClick={PopupState.close}>Logout</MenuItem>
-                    <MenuItem onClick={PopupState.close}>Logout</MenuItem>
-                    <MenuItem onClick={PopupState.close}>Logout</MenuItem>
-                    <MenuItem onClick={PopupState.close}>Logout</MenuItem>
-                    <MenuItem onClick={PopupState.close}>Logout</MenuItem>
-                    <MenuItem onClick={PopupState.close}>Logout</MenuItem>
-                    <MenuItem onClick={PopupState.close}>Logout</MenuItem>
-                    <MenuItem onClick={PopupState.close}>Logout</MenuItem>
-                  </Menu>
-                  <Button
-                    disableRipple
-                    sx={{
-                      my: 2,
-                      margin:0,
-                      padding:2,
-                      color: "white",
-                      display: "block",
-                      ":hover": { color: "#FFAFCF",backgroundColor:"rgba(0, 0, 0, 0)", },
-                    }}
-                  >
-                    <Link className="link-navbar" to={"/abaut"}>Abous us</Link>
-                  </Button>
-                  <Button
-                    disableRipple
-                    sx={{
-                      my: 2,
-                      margin:0,
-                      padding:2,
-                      color: "white",
-                      display: "block",
-                      ":hover": { color: "#F9C7DB",backgroundColor:"rgba(0, 0, 0, 0)", },
-                    }}
-                  >
-                    <Link className="link-navbar" to={"/products/:detailId/:detailName"}>Contact</Link>
-                  </Button>
-                </Box>
-                <Box  sx={{backgroundColor:"#323232d7",width:"200px",height:"44px",borderRadius:"20px",display:"flex",justifyContent:"space-between",alignItems:"center"}}>
-                    <Avatar src={avatar}/>
-                    <Button onClick={()=>navigate("/auth")} disableRipple sx={{color: "white",":hover": { color: "#F9C7DB",backgroundColor:"rgba(0, 0, 0, 0)", },}}><Typography sx={{px:"20px",fontSize:"13px"}}>{token?user:"Login / LogOut"}</Typography></Button>
-                </Box>
-              </Toolbar>
-            </AppBar>
-          </Box>
-        </Fragment>
-      )}
-    </PopupState>
+    <>
+      <Box component={"div"} className="navbar">
+        <Grid container xs={12} sx={{bgcolor:"primary.main"}}>
+          <Grid xs={7}>item 11</Grid>
+          <Grid xs={5}>item 1</Grid>
+        </Grid>
+        <Grid container xs={12}>
+          <Grid xs={4}>item 2</Grid>
+          <Grid xs={4}>item 2</Grid>
+          <Grid xs={4}>item 2</Grid>
+        </Grid>
+        <Grid container xs={12}>
+          item 3
+        </Grid>
+      </Box>
+    </>
   );
 }
