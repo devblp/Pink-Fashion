0a7528fc07a59573db39f924df26053228b00aa8
BlP. <dev.sinanp@gmail.com>
2024-05-12 15:32:58 -0700

    add

--- Frontend/src/Components/Navbar/index.jsx
+++ Frontend/src/Components/Navbar/index.jsx
@@ -15,9 +15,13 @@ import { Fragment } from "react";
 import "./style.css";
 import { useNavigate, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import imageUsa from "../../img/USA.png"
+import imageUsa from "../../img/USA.png";
+import logo from "../../img/logo.png"
 
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import LocalMallIcon from '@mui/icons-material/LocalMall';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Footer() {
   const { token, user, avatar } = useSelector((state) => state.auth);
@@ -33,7 +37,9 @@ export default function Footer() {
           <Grid item xs={7} sx={{ display: "flex", justifyContent: "right" }}>
             <Box sx={{ display: "flex", alignItems: "center" }}>
               <Typography>Get early access on launches and offers.</Typography>
-              <Link to={"/auth"} className="link-navbar">Sign Up For Texts</Link>
+              <Link to={"/auth"} className="link-navbar link-to">
+                Sign Up For Texts
+              </Link>
               <ArrowForwardIcon fontSize="14" />
             </Box>
           </Grid>
@@ -43,18 +49,36 @@ export default function Footer() {
               display: "flex",
               justifyContent: "right",
               alignItems: "center",
-              gap:"10px",
-              paddingRight:"30px"
+              gap: "10px",
+              paddingRight: "30px",
             }}
           >
-            <Avatar src={imageUsa} sx={{width:21,height:15,borderRadius:0}}/>
+            <Avatar
+              src={imageUsa}
+              sx={{ width: 21, height: 15, borderRadius: 0 }}
+            />
             <Typography>USA</Typography>
           </Grid>
         </Grid>
-        <Grid container xs={12} sx={{ height: 75 }}>
-          <Grid xs={4}>item 2</Grid>
-          <Grid xs={4}>item 2</Grid>
-          <Grid xs={4}>item 2</Grid>
+        <Grid container xs={12} sx={{ height: 75 ,display: "flex" ,alignItems:"center" }}>
+          <Grid xs={4}>
+            <Box sx={{ display: "flex", justifyContent: "center",gap:"20px" }}>
+              <Typography className="filter-menu-nav">Women</Typography>
+              <Typography className="filter-menu-nav">Men</Typography>
+              <Typography className="filter-menu-nav">About</Typography>
+              <Typography className="filter-menu-nav">Everworld Stories</Typography>
+            </Box>
+          </Grid>
+          <Grid xs={4}>
+            <Box sx={{ display: "flex", justifyContent: "center" }}><Link to={"/"}><Avatar src={logo} sx={{ width: 108, height: 55, borderRadius: 0 }}/></Link></Box>
+          </Grid>
+          <Grid xs={4}>
+            <Box sx={{ display: "flex", justifyContent: "center",gap:"20px" }}>
+              <Link className="link-navbar-icon"><SearchIcon /></Link>
+              <Link to={"/dashbord"} className="link-navbar-icon"><PermIdentityIcon /></Link>
+              <Link className="link-navbar-icon"><LocalMallIcon /></Link>
+            </Box>
+          </Grid>
         </Grid>
         <Grid container xs={12} sx={{ height: 56 }}>
           item 3
