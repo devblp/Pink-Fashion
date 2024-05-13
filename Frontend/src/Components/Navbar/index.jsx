6402bfeffa39e57a89edf20e55787029bbd38990
BlP. <dev.sinanp@gmail.com>
2024-05-13 14:04:41 -0700

    add

--- Frontend/src/Components/Navbar/index.jsx
+++ Frontend/src/Components/Navbar/index.jsx
@@ -10,22 +10,30 @@ import {
   Icon,
 } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
-import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
-import { Fragment } from "react";
+import {  useState } from "react";
 import "./style.css";
 import { useNavigate, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import imageUsa from "../../img/USA.png";
-import logo from "../../img/logo.png"
+import logo from "../../img/logo.png";
 
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import LocalMallIcon from '@mui/icons-material/LocalMall';
-import PermIdentityIcon from '@mui/icons-material/PermIdentity';
-import SearchIcon from '@mui/icons-material/Search';
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Footer() {
   const { token, user, avatar } = useSelector((state) => state.auth);
   const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState(null);
+  console.log(anchorEl);
+  const open = Boolean(anchorEl);
+  const handelOpenMegaMenu = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handelClosMegaMenu = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <Box component={"div"} className="navbar">
@@ -60,28 +68,119 @@ export default function Footer() {
             <Typography>USA</Typography>
           </Grid>
         </Grid>
-        <Grid container xs={12} sx={{ height: 75 ,display: "flex" ,alignItems:"center" }}>
+        <Grid
+          container
+          xs={12}
+          sx={{ height: 75, display: "flex", alignItems: "center" }}
+        >
           <Grid xs={4}>
-            <Box sx={{ display: "flex", justifyContent: "center",gap:"20px" }}>
-              <Typography className="filter-menu-nav">Women</Typography>
-              <Typography className="filter-menu-nav">Men</Typography>
-              <Typography className="filter-menu-nav">About</Typography>
-              <Typography className="filter-menu-nav">Everworld Stories</Typography>
+            <Box
+              sx={{ display: "flex", justifyContent: "center", gap: "20px" }}
+            >
+              <Link to={""} className="link-navbar-menu">
+                <Typography className="filter-menu-nav">Women</Typography>
+              </Link>
+              <Link to={""} className="link-navbar-menu">
+                <Typography className="filter-menu-nav">Men</Typography>
+              </Link>
+              <Link to={""} className="link-navbar-menu">
+                <Typography className="filter-menu-nav">About</Typography>
+              </Link>
+              <Link to={""} className="link-navbar-menu">
+                {" "}
+                <Typography className="filter-menu-nav">
+                  Everworld Stories
+                </Typography>
+              </Link>
             </Box>
           </Grid>
           <Grid xs={4}>
-            <Box sx={{ display: "flex", justifyContent: "center" }}><Link to={"/"}><Avatar src={logo} sx={{ width: 108, height: 55, borderRadius: 0 }}/></Link></Box>
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+              <Link to={"/"}>
+                <Avatar
+                  src={logo}
+                  sx={{ width: 108, height: 55, borderRadius: 0 }}
+                />
+              </Link>
+            </Box>
           </Grid>
           <Grid xs={4}>
-            <Box sx={{ display: "flex", justifyContent: "center",gap:"20px" }}>
-              <Link className="link-navbar-icon"><SearchIcon /></Link>
-              <Link to={"/dashbord"} className="link-navbar-icon"><PermIdentityIcon /></Link>
-              <Link className="link-navbar-icon"><LocalMallIcon /></Link>
+            <Box
+              sx={{ display: "flex", justifyContent: "center", gap: "20px" }}
+            >
+              <Link className="link-navbar-icon">
+                <SearchIcon />
+              </Link>
+              <Link to={"/dashbord"} className="link-navbar-icon">
+                <PermIdentityIcon />
+              </Link>
+              <Link className="link-navbar-icon">
+                <LocalMallIcon />
+              </Link>
             </Box>
           </Grid>
         </Grid>
-        <Grid container xs={12} sx={{ height: 56 }}>
-          item 3
+        <Grid
+          container
+          xs={12}
+          sx={{ height: 56, display: "flex", justifyContent: "center" }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "30px",
+            }}
+          >
+            <Link to={""} className="link-navbar-menu">
+              <Typography className="filter-menu-nav">
+                Holiday Gifting
+              </Typography>
+            </Link>
+            <Link to={""} className="link-navbar-menu">
+              <Typography className="filter-menu-nav">New Arrivals</Typography>
+            </Link>
+            <Link to={""} className="link-navbar-menu">
+              <Typography className="filter-menu-nav">Best-Sellers</Typography>
+            </Link>
+            <Link to={""} className="link-navbar-menu">
+              <Typography className="filter-menu-nav">
+                Tops & Sweaters
+              </Typography>
+            </Link>
+            <Link to={""} className="link-navbar-menu">
+              <Typography className="filter-menu-nav">Pants & Jeans</Typography>
+            </Link>
+            <Link to={""} className="link-navbar-menu">
+              <Typography className="filter-menu-nav">Outerwear</Typography>
+            </Link>
+            <Link to={""} className="link-navbar-menu">
+              <Typography className="filter-menu-nav">Shoes & Bags</Typography>
+            </Link>
+            <Link to={""} className="link-navbar-menu">
+              <Button
+                className="filter-menu-nav mega-menu-button"
+                sx={{
+                  color: "primary.main",
+                  ":hover": { bgcolor: "primary.ws" },
+                }}
+                onClick={handelOpenMegaMenu}
+              >
+                Sale
+              </Button>
+            </Link>
+            <Menu
+              className="mega-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={open}
+              onClose={handelClosMegaMenu}
+              PaperProps={{sx:{width: '100%' ,left:0, right: 0,display:"block"}}}
+            >
+              <MenuItem >s</MenuItem>
+            </Menu>
+          </Box>
         </Grid>
       </Box>
     </>
