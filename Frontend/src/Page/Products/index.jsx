42e445f0cb6fd2706961977734bedaa31f2cc0c6
BlP. <dev.sinanp@gmail.com>
2024-05-19 15:13:00 -0700

    creat up

--- Frontend/src/Page/Products/index.jsx
+++ Frontend/src/Page/Products/index.jsx
@@ -14,22 +14,34 @@ import {
   FormControlLabel,
   FormLabel,
   Button,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
 } from "@mui/material";
-
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { Link, useParams } from "react-router-dom";
 import fetchData from "../../Utils/fetchData";
 import CardProducts from "../../Components/CardProducts";
 import SkeletonProduct from "./Skeleton";
-import FilterComponent from "./Filters";
+
+
 export default function Products() {
 
+  
   const [categoryNames, setCategoryNames] = useState();
-  const [valeuNames, setValeuNames] = useState(null);
   const [categoryColor, setCategoryColor] = useState();
   const [categorySize, setCategorySize] = useState();
   const [products, setProducts] = useState();
   const { catgoryId, catgoryName } = useParams();
+  const [filters, setFilters] = useState({
+    categories: [],
+    colors: [],
+    sizes: {
+      waist: [],
+      clothing: [],
+    },
+  });
 
   const fetchDataAsync = async (url) => {
     try {
@@ -49,13 +61,8 @@ export default function Products() {
       setCategorySize(nameCategory.slice(12, 16));
       const productsData = await fetchDataAsync(
         `products?populate=*${
-          catgoryId === "all-product"
-            ? ""
+          catgoryId === "all-product"? ""
             : `&filters[categories][id][$eq]=${catgoryId}`
-        }${
-          valeuNames
-            ? `&filters[categories][id][$eq]=${valeuNames !== "women" ? 1 : 2}`
-            : ""
         }`
       );
 
@@ -72,18 +79,196 @@ export default function Products() {
         )) || [];
       setProducts(product);
     })();
-  }, [catgoryId, valeuNames]);
+  }, [catgoryId, filters.categories]);
   const lengthProduct = products?.length;
 
-
-
   // --------filter start
-  
+
+  const [dataCategorie, setDataCategorie] = useState([]);
+  console.log(dataCategorie);
+  console.log(filters.categories);
+
+  useEffect(() => {
+    (async () => {
+      const categories = await fetchDataAsync(`categories?populate=*`);
+      setDataCategorie(categories);
+    })();
+  }, []);
+
+  const handleCategoryChange = async (event) => {
+    const { checked, value } = event.target;
+    setFilters((prev) => ({
+      ...prev,
+      categories: checked
+        ? [...prev.categories, value]
+        : prev.categories.filter((cat) => cat !== value),
+    }));
+    
+  };
+  const handleColorChange = (event) => {
+    const { checked, value } = event.target;
+    setFilters((prev) => ({
+      ...prev,
+      colors: checked
+        ? [...prev.colors, value]
+        : prev.colors.filter((color) => color !== value),
+    }));
+  };
+
+  const handleSizeChange = (event, type) => {
+    const { checked, value } = event.target;
+    setFilters((prev) => ({
+      ...prev,
+      sizes: {
+        ...prev.sizes,
+        [type]: checked
+          ? [...prev.sizes[type], value]
+          : prev.sizes[type].filter((size) => size !== value),
+      },
+    }));
+  };
+
+  const handleFilterReset = () => {
+    setFilters({
+      categories: [],
+      colors: [],
+      sizes: {
+        waist: [],
+        clothing: [],
+      },
+    });
+  };
+
+  const categoriese =
+    dataCategorie?.map((e) => (
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={filters.categories.includes(e?.attributes?.name)}
+            onChange={handleCategoryChange}
+            value={e?.attributes?.name}
+          />
+        }
+        label={e?.attributes?.name}
+        key={e?.id}
+      />
+    )) || [];
+  const colors = ""
+  const size = ""
   return (
     <>
       <Grid container xs={12} sx={{ p: 10 }}>
         <Grid xs={3}>
-          <FilterComponent/>
+          <Grid container spacing={2} direction="column">
+            <Grid item>
+              <Accordion expanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                  <FormLabel>Category</FormLabel>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <FormGroup>{categoriese.slice(0, 8)}</FormGroup>
+                  <Button>View More +</Button>
+                </AccordionDetails>
+              </Accordion>
+            </Grid>
+            <Grid item>
+              <Accordion expanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                  <FormLabel>Color</FormLabel>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <FormGroup row>
+                    {[
+                      "black",
+                      "green",
+                      "blue",
+                      "brown",
+                      "orange",
+                      "pink",
+                      "red",
+                      "white",
+                      "yellow",
+                    ].map((color) => (
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            checked={filters.colors.includes(color)}
+                            onChange={handleColorChange}
+                            value={color}
+                          />
+                        }
+                        label={
+                          <div
+                            style={{
+                              width: 20,
+                              height: 20,
+                              backgroundColor: color,
+                            }}
+                          ></div>
+                        }
+                        key={color}
+                      />
+                    ))}
+                  </FormGroup>
+                  <Button>View More +</Button>
+                </AccordionDetails>
+              </Accordion>
+            </Grid>
+            <Grid item>
+              <Accordion expanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                  <FormLabel>Size</FormLabel>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <FormLabel component="legend">Waist</FormLabel>
+                  <FormGroup row>
+                    {["36", "38", "40", "42", "44", "46", "48", "50"].map(
+                      (size) => (
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={filters.sizes.waist.includes(size)}
+                              onChange={(e) => handleSizeChange(e, "waist")}
+                              value={size}
+                            />
+                          }
+                          label={size}
+                          key={size}
+                        />
+                      )
+                    )}
+                  </FormGroup>
+                  <FormLabel component="legend">Clothing</FormLabel>
+                  <FormGroup row>
+                    {["XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"].map(
+                      (size) => (
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={filters.sizes.clothing.includes(size)}
+                              onChange={(e) => handleSizeChange(e, "clothing")}
+                              value={size}
+                            />
+                          }
+                          label={size}
+                          key={size}
+                        />
+                      )
+                    )}
+                  </FormGroup>
+                </AccordionDetails>
+              </Accordion>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleFilterReset}
+              >
+                Reset Filters
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
         <Grid container xs={9} sx={{ px: 2 }}>
           <Grid xs={12}>
